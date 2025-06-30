c9388b09ba2c8392e476fd5a2b79d484ad26d156
Siva kiran <sivakiran7171@gmail.com>
2025-06-30 19:22:09 +0530

    main

--- run_pipeline.py
+++ run_pipeline.py
@@ -2,4 +2,4 @@ from pipelines.training_pipeline import train_pipeline
 
 if __name__=="__main__":
     #run pipe line
-    train_pipeline(data_path=".data\olist_customers_dataset.csv")
+    train_pipeline(data_path="D:\Anonymous\Machine learning and Deep learning\Machinelearning Ops\data\olist_customers_dataset.csv")
